79e0ea29afdf22e555e885defa6745bdf9c623bb
Cai900205 <1198430034@qq.com>
2015-09-02 15:57:36 +0800

    0902/mm

---
+++ 0902/master_mbound/objs_powerpc64/master_md_fvl_srio.d
@@ -0,0 +1,34 @@
+apps/master_mbound/objs_powerpc64/master_md_fvl_srio.o: \
+ apps/master_mbound/fvl_srio.c apps/master_mbound/fvl_srio.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/compat.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/conf.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/compat_list.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/of.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/dma_mem.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/fsl_dma.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/fsl_srio.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/apps/include/atb_clock.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/sysroots/t4240qds-64b/usr/include/editline/readline.h \
+ apps/master_mbound/fvl_common.h
+
+apps/master_mbound/fvl_srio.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/compat.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/conf.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/compat_list.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/of.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/dma_mem.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/fsl_dma.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/fsl_srio.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/apps/include/atb_clock.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/sysroots/t4240qds-64b/usr/include/editline/readline.h:
+
+apps/master_mbound/fvl_common.h:
---
+++ 0902/slave_mbound/objs_powerpc64/slave_md_main.d
@@ -0,0 +1,34 @@
+apps/slave_mbound/objs_powerpc64/slave_md_main.o: \
+ apps/slave_mbound/main.c apps/slave_mbound/fvl_common.h \
+ apps/slave_mbound/fvl_srio.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/compat.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/conf.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/compat_list.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/of.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/dma_mem.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/fsl_dma.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/fsl_srio.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/apps/include/atb_clock.h \
+ /home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/sysroots/t4240qds-64b/usr/include/editline/readline.h
+
+apps/slave_mbound/fvl_common.h:
+
+apps/slave_mbound/fvl_srio.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/compat.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/conf.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/compat_list.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/of.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/dma_mem.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/fsl_dma.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/include/usdpaa/fsl_srio.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/work/t4240qds_64b-fsl-linux/usdpaa/git-r0/git/apps/include/atb_clock.h:
+
+/home/freescale/SDK/QorIQ-SDK-V1.6-20140619-yocto/build_t4240qds-64b_release/tmp/sysroots/t4240qds-64b/usr/include/editline/readline.h:
